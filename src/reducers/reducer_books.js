73030ede889f20de0b0088d1060d316de84880d9
Malind Kowalski <malind.nikulski@gmail.com>
2018-07-11 10:17:05 -0600

    add new reducer file, import BooksReducer to reducers/index.js

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,11 @@
+//Step 1: create the reducer
+//Step 2: wire it into the application
+
+export default function() {
+  return [
+    {title: 'JavaScript: The Good Parts'},
+    {title: 'The Martian'},
+    {title: 'Red Sparrow'},
+    {title: 'Eloquent Ruby'},
+  ]
+}
