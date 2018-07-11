014cbd33b37acca67c9dd8e1c82435fb6c21187f
Malind Kowalski <malind.nikulski@gmail.com>
2018-07-11 12:44:49 -0600

    import functions into book_list, comments regarding new function

--- src/containers/book_list.js
+++ src/containers/book_list.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
 class BookList extends Component {
   renderList() {
@@ -26,4 +27,13 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(BookList);
+//anything returned from this function will end up as props on the BookList container
+function mapDispatchToProps(dispatch) {
+  //whenever selectBook is called, result should be passed to all reducers
+  return bindActionCreators({ selectBook: selectBook }, dispatch)
+}
+
+//promote BookList from a component to a container
+//it needs to know about this new dispatch method, selectBook
+//make it available as a prop
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
