63accc9f8971fc89570d4751e5e0c6c6bf44ddbb
Malind Kowalski <malind.nikulski@gmail.com>
2018-07-11 11:26:46 -0600

    remover minor comments

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,6 @@
 import { combineReducers } from 'redux';
 import BooksReducer from './reducer_books';
 
-//mapping of the state
 const rootReducer = combineReducers({
   books: BooksReducer
 });
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,6 +1,3 @@
-//Step 1: create the reducer
-//Step 2: wire it into the application
-
 export default function() {
   return [
     {title: 'JavaScript: The Good Parts'},
