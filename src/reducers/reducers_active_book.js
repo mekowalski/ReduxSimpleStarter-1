827443476ff8afd25c9bd608c6a1b1a5504931d5
Malind Kowalski <malind.nikulski@gmail.com>
2018-07-12 13:18:48 -0600

    create reducers_active_book

---
+++ src/reducers/reducers_active_book.js
@@ -0,0 +1,5 @@
+//all reducers get 2 arguments: current state and action
+//state argument is not application state, only the state this reducer is responsible for
+export default function(state, action) {
+
+}
