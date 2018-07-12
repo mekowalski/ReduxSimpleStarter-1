d349da065504964ee011d4cef044c55e4052e8f0
Malind Kowalski <malind.nikulski@gmail.com>
2018-07-12 13:24:19 -0600

    switch/case statement. saving before reboot

--- src/reducers/reducers_active_book.js
+++ src/reducers/reducers_active_book.js
@@ -1,5 +1,12 @@
 //all reducers get 2 arguments: current state and action
 //state argument is not application state, only the state this reducer is responsible for
-export default function(state, action) {
+//when state argument comes undefined, return null
+export default function(state = null, action) {
+  //state += 1 the first time the action is triggered then state would add 1, then goes up to 2, 3 etc
+  switch(action.type) {
+  case 'BOOK_SELECTED':
+    return action.payload;
+  }
 
+  return state
 }
