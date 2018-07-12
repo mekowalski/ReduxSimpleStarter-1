1ed06a1b4ca63091c0a8a1d8bd2033ec606cba3a
Malind Kowalski <malind.nikulski@gmail.com>
2018-07-12 13:45:41 -0600

    create book_detail container, modify app.js and reducer active book

--- src/reducers/reducer_active_book.js
+++ src/reducers/reducer_active_book.js
@@ -1,8 +1,4 @@
-//all reducers get 2 arguments: current state and action
-//state argument is not application state, only the state this reducer is responsible for
-//when state argument comes undefined, return null
 export default function(state = null, action) {
-  //state += 1 the first time the action is triggered then state would add 1, then goes up to 2, 3 etc
   switch(action.type) {
   case 'BOOK_SELECTED':
     return action.payload;
