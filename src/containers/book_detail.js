ef51ef30f0213fcb0e37dc4f53cdfe9686492833
Malind Kowalski <malind.nikulski@gmail.com>
2018-07-13 14:20:44 -0600

    minor details, code is not executing in the browser

--- src/containers/book_detail.js
+++ src/containers/book_detail.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-class BookDetail extends Component{
+class BookDetail extends Component {
   render() {
-    if(!this.props.book) {
-      return <div>Select a book to get started</div>
+    if (!this.props.book) {
+      return <div>Select a book to get started</div>;
     }
-    
+
     return (
       <div>
         <h3>Details for: </h3>
