6244e3d4a7ed68dbb4f61052a1be3a2e84cdc32d
Malind Kowalski <malind.nikulski@gmail.com>
2018-07-16 15:38:02 -0600

    review notes and comments

--- src/reducers/reducer_active_book.js
+++ src/reducers/reducer_active_book.js
@@ -6,3 +6,23 @@ export default function(state = null, action) {
 
   return state;
 }
+
+//first point review: REDUX IS IN CHARGE OF MANAGING THE APPLICATION STATE
+//STATE IS A SINGLE PLAIN JAVASCRIPT OBJECT
+//application state is different than component state
+
+//second point review:  APPLICATION STATE IS FROMED BY REDUCERS
+//reducers get tied together wit combineReducers method inside of reducers/index
+//for each key, assign one reducer responsible for creating a piece of state
+
+//third point review: REDUCERS ARE IN CHARGE OF CHANGING APPLICATION STATE OVER TIME USING ACTIONS
+//whenever action is dispatched it flows through all different reducer in application
+
+//final point review: ACTION AND ACTION CREATOR
+//action creators are simple functions that return an action
+//action is plain JavaScript object, with type defined
+
+
+//if i don't understand the action cycle yet, don't worry, i will over time.  this is challenging and i
+//will get this.  be kind to the brain, it's taking in so much knowledge.
+//redux is a cycle
